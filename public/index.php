a8769c38135179d9d24248fea76f0874633c2e8a
Zyvie292 <zyviedelapena292@gmail.com>
2025-03-08 13:22:16 +0800

    Moved index.php and assets to public/

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pamilya - eCommerce Website</title>
 
-  <link rel="shortcut icon" href="./assets/images/logo/Pamilya.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="/assets/images/logo/Pamilya.ico" type="image/x-icon">
 
   <!-- <link rel="stylesheet" href="./assets/css/style.css"> -->
-  <link rel="stylesheet" href="./assets/css/style-prefix.css">
+  <link rel="stylesheet" href="/assets/css/style-prefix.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"rel="stylesheet">
